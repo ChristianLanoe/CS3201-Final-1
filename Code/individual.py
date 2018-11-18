14b1e0091a3ca1bf522a2ac4afc728907665a877
Eric <EricRoy.Elli@gmail.com>
2018-11-18 17:34:42 -0330

    Individual.individual -> Individual.path

--- Code/individual.py
+++ Code/individual.py
@@ -1,10 +1,10 @@
 class Individual:
 
 
-    def __init__(self, individual, fitness):
-        self.individual = individual
+    def __init__(self, path, fitness):
+        self.path = path
         self.fitness = fitness
 
 
     def __str__(self):
-        return ("Individual: {} \nFitness: {}".format(self.individual, self.fitness))
+        return ("Path: {} \nFitness: {}".format(self.path, self.fitness))
