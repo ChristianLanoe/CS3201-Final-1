7f2b0c1145e97883c163dab151451790592a11e1
Eric <EricRoy.Elli@gmail.com>
2018-11-29 10:02:50 -0330

    shell script to generate csv files

--- Code/run_tests.sh
+++ Code/run_tests.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-for i in {1..100}
+for i in {1..30}
 do
-    python3 main.py WesternSahara r TS\ With\ SCX\(0.9\)\ Inversion\(0.3\)\ Mu\+\ Lambda\ 100.csv
+    python3 main.py WesternSahara s 1000\ SCX100-PMX\(0.9\)\ Inversion\(0.2\).csv
 done
+# python3 main.py Uruguay s SCX100-PMX\(0.9\)\ Inversion\(0.2\).csv
+
+# python3 main.py WesternSahara r 0\ SCX100-\>\PMX\(0.9\)\ Inversion\(0.2\).csv
+
+osascript -e 'tell app "System Events" to display dialog "Done"'
 
 echo "done"
