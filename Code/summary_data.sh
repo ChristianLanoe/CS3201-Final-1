74da99bab6173d94bab6c9442e8f45d69d2b84a7
Eric <EricRoy.Elli@gmail.com>
2018-12-06 13:28:37 -0330

    Separate scripts to generate run and summary data

--- Code/summary_data.sh
+++ Code/summary_data.sh
@@ -3,9 +3,6 @@ for i in {1..30}
 do
     python3 main.py WesternSahara s 1000\ SCX100-PMX\(0.9\)\ Inversion\(0.2\).csv
 done
-# python3 main.py Uruguay s SCX100-PMX\(0.9\)\ Inversion\(0.2\).csv
-
-# python3 main.py WesternSahara r 0\ SCX100-\>\PMX\(0.9\)\ Inversion\(0.2\).csv
 
 osascript -e 'tell app "System Events" to display dialog "Done"'
 
